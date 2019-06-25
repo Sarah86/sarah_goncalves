cffef515650eff7ab039532d362f75f2b9d82405
Sarah86 <sarah.goncalves@gmail.com>
2019-06-26 00:39:10 +0200

    new website based on starter

---
+++ gatsby-config.js
@@ -0,0 +1,63 @@
+const contentful = require('contentful');
+const manifestConfig = require('./manifest-config');
+require('dotenv').config();
+
+const { ACCESS_TOKEN, SPACE_ID, ANALYTICS_ID } = process.env;
+
+const client = contentful.createClient({
+  space: SPACE_ID,
+  accessToken: ACCESS_TOKEN,
+});
+
+const getAboutEntry = entry => entry.sys.contentType.sys.id === 'about';
+
+const plugins = [
+  'gatsby-plugin-react-helmet',
+  {
+    resolve: 'gatsby-plugin-manifest',
+    options: manifestConfig,
+  },
+  'gatsby-plugin-styled-components',
+  {
+    resolve: 'gatsby-plugin-google-fonts',
+    options: {
+      fonts: ['cabin', 'Open Sans'],
+    },
+  },
+  {
+    resolve: 'gatsby-source-contentful',
+    options: {
+      spaceId: 'k721jnxe76g2',
+      accessToken: 'tBzRxs0yu93abrx7uf0kP1q8e21nXHDW8oRM2teXotE',
+    },
+  },
+  'gatsby-transformer-remark',
+  'gatsby-plugin-offline',
+];
+
+module.exports = client.getEntries().then(entries => {
+  const { mediumUser } = entries.items.find(getAboutEntry).fields;
+
+  plugins.push({
+    resolve: 'gatsby-source-medium',
+    options: {
+      username: mediumUser || '@medium',
+    },
+  });
+
+  if (ANALYTICS_ID) {
+    plugins.push({
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        trackingId: 'UA-142747868-1',
+      },
+    });
+  }
+
+  return {
+    siteMetadata: {
+      isMediumUserDefined: !!mediumUser,
+    },
+    plugins,
+  };
+});
