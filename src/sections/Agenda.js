4ce2b07fd11ea40451437fa3d52ab9f4b30bd559
Sarah86 <sarah.goncalves@gmail.com>
2019-10-07 01:21:10 +0200

    added calendly and fixed bugs

--- src/sections/Agenda.js
+++ src/sections/Agenda.js
@@ -4,8 +4,8 @@ import Calendly from '../components/Calendly';
 import Section from '../components/Section';
 
 const Agenda = () => (
-  <Section.Container id="calendar">
-    <Section.Header name="Calendar" icon="📅" Box="calendar" />
+  <Section.Container id="Schedule a Chat">
+    <Section.Header name="Schedule a Chat" icon="📅" Box="Schedule a Chat" />
     If you want to talk to me, just set here below an appointment. Easy as that!
     ⚡
     <Calendly />
